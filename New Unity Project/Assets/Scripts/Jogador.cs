81bea168ba1c45ec19b4f0e997fce681fd664df8
LordCefer <ceferfg14@gmail.com>
2020-06-16 01:03:36 -0300

    Animação

--- New Unity Project/Assets/Scripts/Jogador.cs
+++ New Unity Project/Assets/Scripts/Jogador.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Controlador2D))]
 public class Jogador : MonoBehaviour
@@ -23,10 +24,16 @@ public class Jogador : MonoBehaviour
     public int vidaMaxima = 3;
     public int vidaAtual;
     public int moedas;
+    public Direcao direcaoOlhar = Direcao.Direita;
+    private Animator animador;
+    public bool estaPulando;
+    public bool estaAndando;
+    public bool estaParado;
 
     void Awake()
     {
         controlador = GetComponent<Controlador2D>();
+        animador = GetComponent<Animator>();
 
         // Você consegue entender o que estamos fazendo abaixo?
         // Dica: S = S0 + v0.t + (a.t²)/2
@@ -57,6 +64,32 @@ public class Jogador : MonoBehaviour
         {
             velocidade.y = velocidadePulo;
         }
+
+        if (velocidade.y != 0)
+        {
+            estaPulando = true;
+            estaAndando = false;
+            estaParado = false;
+        }
+        else
+        {
+            if (inputHorizontal == 0)
+            {
+                estaAndando = false;
+                estaParado = true;
+            }
+            else
+            {
+                estaAndando = true;
+                estaParado = false;
+            }
+
+            estaPulando = false;
+        }
+        
+        animador.SetBool("estaParado", estaParado);
+        animador.SetBool("estaAndando", estaAndando);
+        animador.SetBool("estaPulando", estaPulando);
             
         // Você consegue entender o que o código abaixo faz?
         // Tente substituír as duas linhas abaixo por a seguinte:
@@ -70,10 +103,18 @@ public class Jogador : MonoBehaviour
 
         // E finalmente move nosso personagem.
         controlador.Mover(velocidade * Time.deltaTime);
+
+        AtualizaOlhar(inputHorizontal);
     }
 
     public void MudarVida(int valor)
     {
+        if (Mathf.Sign(valor) == -1)
+        {
+            velocidade.x = 30f * (direcaoOlhar == Direcao.Direita ? -1 : 1);
+            animador.SetTrigger("levouDano");
+        }
+
         vidaAtual += valor;
 
         if (vidaAtual > vidaMaxima)
@@ -89,30 +130,21 @@ public class Jogador : MonoBehaviour
     
     private void Morrer()
     {
-        Destroy(this.gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void OnTriggerEnter2D(Collider2D colisao)
     {
         if (colisao.tag == "Inimigo")
         {
-            if (colisao.transform.position.y > transform.position.y)
+            if ((transform.position.y - controlador.colisor.size.y/2) > colisao.transform.position.y)
             {
-                print("Levou dano");
-                // Levar dano
-                MudarVida(-1);
-                velocidade.x += -10;
-                controlador.Mover(velocidade * Time.deltaTime);
-            }
-
-            if (colisao.transform.position.y < transform.position.y)
-            {
-                // Dar dano
-                print("Deu dano");
-                colisao.SendMessage("MudarVida", -1);
+                colisao.gameObject.SendMessage("MudarVida", -1);
                 velocidade.y = velocidadePulo;
                 controlador.Mover(velocidade * Time.deltaTime);
             }
+            else
+                MudarVida(-1);
         }
 
         if (colisao.tag == "Moeda")
@@ -120,5 +152,40 @@ public class Jogador : MonoBehaviour
             moedas++;
             Destroy(colisao.gameObject);
         }
+
+        if (colisao.tag == "Limite")
+        {
+            Morrer();
+        }
+
+        if (colisao.tag == "FimFase")
+        {
+            colisao.gameObject.SendMessage("MudarNivel");
+        }
+    }
+
+    private void AtualizaOlhar(float inputHorizontal)
+    {
+        // Checa se o Input é pra direita
+        if (inputHorizontal == 1)
+        {
+            // Checa se o olhar NÃO é pra direita, e então muda
+            if (direcaoOlhar != Direcao.Direita)
+            {
+                direcaoOlhar = Direcao.Direita;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
+        else if (inputHorizontal == -1)
+        {
+            // Checa se o olhar NÃO é pra esquerda, e então muda
+            if (direcaoOlhar != Direcao.Esquerda)
+            {
+                direcaoOlhar = Direcao.Esquerda;
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+        }
     }
 }
+
+public enum Direcao {Direita, Esquerda};
